f3e87407d18d93577308ef7e6d353f696eb2ecc8
Jeongwoo Son <jeson@c2r7s5.42seoul.kr>
2022-06-07 19:45:41 +0900

    reset

--- srcs/requirements/mariadb/tools/start.sh
+++
@@ -1,9 +0,0 @@
-service mysql start;
-echo "CREATE DATABASE $DB;" | mysql;
-echo "CREATE USER '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASSWORD';" | mysql;
-echo "GRANT ALL PRIVILEGES ON $DB.* TO '$USER_NAME'@'%';" | mysql;
-echo "FLUSH PRIVILEGES;" | mysql;
-
-mysql $DB -u $USER_NAME -p$USER_PASSWORD < /user_data.sql; 
-
-mysqld;
--- srcs/requirements/wordpress/tools/start.sh
+++
@@ -1,6 +0,0 @@
-sed -i "s/database_name_here/$DB/g" /var/www/html/wp-config.php; 
-sed -i "s/username_here/$USER_NAME/g" /var/www/html/wp-config.php; 
-sed -i "s/password_here/$USER_PASSWORD/g" /var/www/html/wp-config.php; 
-sed -i "s/localhost/$DB_KIND/g" /var/www/html/wp-config.php;
-
-/usr/sbin/php-fpm7.3;
