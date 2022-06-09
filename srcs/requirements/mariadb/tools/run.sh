a35f4bdfbcbf87f555cd854da6b25d8479730fa1
Jeongwoo Son <jeson@c2r7s5.42seoul.kr>
2022-06-09 19:20:12 +0900

    mariadb

---
+++ srcs/requirements/mariadb/tools/run.sh
@@ -0,0 +1,19 @@
+# db 설정
+
+# dir 만들어져있지 않을때 실행
+if [ ! -d /var/lib/mysql/$WP_DB_NAME ]; then
+	# mysql 시작
+    service mysql start
+    mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
+	# @'%' -> 외부와 통신 가능
+    mysql -e "CREATE USER '$DB_ADMIN_NAME'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD';"
+    mysql -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$DB_ADMIN_NAME'@'%';"
+	# @'localhost' -> 외부와 통신 불가
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW';"
+    # 변경사항 적용
+	mysql -e "FLUSH PRIVILEGES;"
+    mysqladmin -u root -p$MYSQL_ROOT_PW shutdown
+fi
+
+# mariadb foreground 실행 / background로 실행은 & 옵션 사용
+exec mysqld_safe
