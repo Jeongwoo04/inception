3a162ba7dd8106ca9b40d062d13ddd3ca5d61692
Jeongwoo Son <jeson@c1r8s7.42seoul.kr>
2022-06-16 16:53:33 +0900

    mariadb run.sh

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -12,10 +12,10 @@ if [ ! -d /var/lib/mysql/$WP_DB_NAME ]; then
 	# @'localhost' -> 외부와 통신 불가
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW';"
 	# 변경사항 적용
-	sleep 3
 	mysql -e "FLUSH PRIVILEGES;"
 	service mysql stop
 fi
 
 # mariadb foreground 실행 / background로 실행은 & 옵션 사용
+sleep 5
 exec mysqld_safe
