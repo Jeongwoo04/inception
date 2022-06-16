36aac9864c32347d426c750a0cc07f7bfff003c8
Jeongwoo Son <jeson@c1r8s7.42seoul.kr>
2022-06-16 19:43:13 +0900

    wansung

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -6,11 +6,10 @@ if [ ! -d /var/lib/mysql/$WP_DB_NAME ]; then
 	# mysql 시작
 	service mysql start
 	mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
-	# @'%' -> 외부와 통신 가능
+	# @'%' -> host를 %로 주면 모든 외부 ip에서 접속 가능
 	mysql -e "CREATE USER '$DB_ADMIN_NAME'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD';"
-	# 모든 권한 부여 / bind-address 설정 없어도 해당 계정은 모든 ip에서 접근 가능
+	# 모든 권한 부여
 	mysql -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$DB_ADMIN_NAME'@'%';"
-	# @'localhost' -> 외부와 통신 불가
 	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW';"
 	# 변경사항 적용
 	mysql -e "FLUSH PRIVILEGES;"
