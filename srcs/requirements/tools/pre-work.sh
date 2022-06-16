cd1b11b3496053455e010be4c81dd0d195103859
Jeongwoo Son <jeson@c1r8s7.42seoul.kr>
2022-06-16 14:01:46 +0900

    pre-work.sh

--- srcs/requirements/tools/pre-work.sh
+++ srcs/requirements/tools/pre-work.sh
@@ -3,8 +3,8 @@
 sudo mkdir -p /home/jeson/data/wordpress
 sudo mkdir -p /home/jeson/data/mariadb
 
-if [ ! -e /etc/pre-work.txt ]; then
-	sudo touch /etc/pre-work.txt
+if [ ! -e /etc/.pre-work.txt ]; then
+	sudo touch /etc/.pre-work.txt
 	sudo chmod 777 /etc/hosts;
 	sudo echo '127.0.0.1 jeson.42.fr' >> /etc/hosts;
 	sudo echo '127.0.0.1 www.jeson.42.fr' >> /etc/hosts;
